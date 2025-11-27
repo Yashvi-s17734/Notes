66190a9851f22e66966dced808c56f4623f82328
Yashvi-s17734 <yashvipshah80@gmail.com>
2025-11-27 11:24:22 +0530

    Fix port for Render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,14 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  });
-
-  await app.listen(3001);
-  console.log("Backend running at http://localhost:3001");
+  app.enableCors();
+  await app.listen(process.env.PORT || 3001);
+  console.log(`Backend running on port ${process.env.PORT || 3001}`);
 }
-
 bootstrap();
